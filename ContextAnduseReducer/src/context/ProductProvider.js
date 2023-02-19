a821b8349a87e05dcd05b1c0dd09e6fabb287d17
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-02-19 18:37:02 +0600

    Context and useReducer Basic Setup

---
+++ ContextAnduseReducer/src/context/ProductProvider.js
@@ -0,0 +1,32 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const PRODUCT_CONTEXT = createContext();
+
+const ProductProvider = ({children}) => {
+    
+  const [data,setData] = useState([]);
+
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/products")
+    .then(res=> res.json())
+    .then((data) => setData(data))
+  },[])
+
+ const value = {
+    data,
+  }
+  return (
+    <PRODUCT_CONTEXT.Provider value={value}>
+            {children}
+    </PRODUCT_CONTEXT.Provider>
+  )
+}
+
+
+export const useProducts = ()=>{
+    const context = useContext(PRODUCT_CONTEXT);
+    return context;
+}
+
+export default ProductProvider
