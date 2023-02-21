964da7d63afef2cc936df152e790dd0b1711ecbb
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-02-21 10:06:24 +0600

    Serverless Rendering

--- Simple_Redux/src/pages/Home.js
+++ Simple_Redux/src/pages/Home.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from './../components/ProductCard';
 
 const Home = () => {
+ const [products,setProducts] = useState();
+
+ useEffect(()=>{
+    fetch("products.json")
+    .then((res)=>res.json())
+    .then((data)=>setProducts(data));
+ },[]);
+ console.log(products);
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+        {
+          products ? products.map((product)=>(<ProductCard product={product} key={product._id}/>)): (<p>Something went wrong</p>)
+        }
     </div>
   );
 };
